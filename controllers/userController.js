029d1d69524914a3a88aa3a0883eb8d089789363
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-02-25 12:55:49 -0800

    create routes index

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,4 +2,8 @@ const { User, Thought } = require("../models");
 
 const getUsers = (req, res) => {
     User.find()
+        .then((users) => res.json(users))
+        .catch((err) => res.status(500).json(err));
 }
+
+module.exports = { getUsers };
