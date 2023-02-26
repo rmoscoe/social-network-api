3bc5569f1a208b1036c7be8cd51881f49369cdff
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-02-25 21:55:05 -0800

    complete userRoutes with remaining routes and methods

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
-const { getUsers } = require("../../controllers/userController");
+const { getUsers, getOneUser, createUser, updateUser, deleteUser, addFriend, removeFriend } = require("../../controllers/userController");
 
-router.route("/").get(getUsers);
+router.route("/").get(getUsers).post(createUser);
+router.route("/:userId").get(getOneUser).put(updateUser).delete(deleteUser);
+router.route("/:userId/friends:friendId").post(addFriend).delete(removeFriend);
 
 module.exports = router;
